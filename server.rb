6b095b6c2dcc346e557acbfc7a8022c983f3cf06
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-07 23:29:05 -0400

    setup the array as instance variable in the favourites route

--- server.rb
+++ server.rb
@@ -14,3 +14,8 @@ get '/about_me' do
   @interests = ['cats', 'art', 'music', 'films', 'coffee']
   erb :about
 end
+
+get '/favourites' do
+  @fav_links = ["https://www.google.ca", "https://www.hotmail.com", "https://www.github.com", "https://ruby-doc.org/core-2.4.1/", "https://www.youtube.com"]
+  erb :favourites
+end
